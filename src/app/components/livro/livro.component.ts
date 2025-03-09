d0f707de5429cf0c2faf2971fc7d550ca201c824
caruazu <gustavocaruazu@gmail.com>
2025-03-09 17:00:45 -0300

    interface e evento de favoritar

--- src/app/components/livro/livro.component.ts
+++ src/app/components/livro/livro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Livro } from '../../types/livro';
 
 @Component({
   selector: 'app-livro',
@@ -7,8 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './livro.component.css',
 })
 export class LivroComponent {
-  livro = {
+  livro: Livro = {
+    imagem: 'https://m.media-amazon.com/images/I/81hCVEC0ExL._SL1500_.jpg',
     titulo: 'O Senhor dos Anéis',
     autor: 'J. R. R. Tolkien',
+    favorito: false,
+    genero: 'Ficção'
   };
+
+  favoritar() {
+    this.livro.favorito = !this.livro.favorito;
+  }
 }
---
+++ src/app/types/livro.ts
@@ -0,0 +1,7 @@
+export interface Livro {
+  imagem: string;
+  titulo: string;
+  autor: string;
+  genero: string;
+  favorito: boolean;
+}
